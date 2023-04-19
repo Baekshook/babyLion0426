38b14ce5d961800537d39dd6ff7181b19455c22c
Baekshook <ecgo0106@naver.com>
2023-04-19 15:50:35 +0900

    230419

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import { ChakraProvider, Box } from "@chakra-ui/react";
-import { useState } from "react";
+import { ChakraProvider, Box, Flex } from "@chakra-ui/react";
+import { createContext, useState } from "react";
 import A from "@components/A";
+
+export const AppContext = createContext();
 
 function App() {
   const [count, setCount] = useState(0);
+  const [names, setNames] = useState([]);
+
+  function handleAddNames(option) {
+    if (!option) {
+      alert("값을 입력하세요!");
+    } else {
+      setNames([...names, "  " + option]);
+    }
+  }
 
   return (
     <>
       <ChakraProvider>
-        <Box bgColor={"blue.100"}>
-          <Box mt={"30px"}>Hello, React</Box>
-          <A count={count} setCount={setCount} />
-        </Box>
+        <AppContext.Provider value={{ count, setCount, names, handleAddNames }}>
+          <Box bgColor={"blue.100"}>
+            <Flex
+              justifyContent={"center"}
+              alignItems={"center"}
+              direction={"column"}
+              mt={"120px"}
+            >
+              <Box mt={"30px"}>Hello, React</Box>
+              <A />
+            </Flex>
+          </Box>
+        </AppContext.Provider>
       </ChakraProvider>
     </>
   );
--- src/components/A.jsx
+++ src/components/A.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 import B from "./B";
 
-export default function A(props) {
+export default function A() {
   return (
     <>
       <Box w={100} h={100} bgColor="red.500">
         A
       </Box>
-      <B count={props.count} setCount={props.setCount} />
+      <B />
     </>
   );
 }
